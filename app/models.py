c4cea280bf021dc10da1b175f58fdbcfb5202c8c
Killian <davittk@tcd.ie>
2018-04-30 22:17:55 +0100

    Updated to conform to slides

--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ from app import db
 class User(db.Model):
     user_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
-    password_hash = db.Column(db.String)
+    email = db.Column(db.String)
 
     @property
     def is_authenticated(self):
--- app/views.py
+++ app/views.py
@@ -1,6 +1,5 @@
 """ Main views for the stats application
 """
-import bcrypt
 
 from flask_login import login_user, login_required
 from flask import render_template, redirect, url_for, request
